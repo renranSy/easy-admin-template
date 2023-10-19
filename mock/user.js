41eb1b9f8a2d2e2482c89c4fef6bf93ca3069c37
sunyang <2480901422@qq.com>
2023-10-20 00:57:50 +0800

    fix🐛: mock完善

--- mock/user.js
+++ mock/user.js
@@ -22,7 +22,12 @@ export default [
         code: 200,
         data: {
           token:
-            'eyJ0eXAiOiJKV1QiLCJhAbDciJIUzI1NiJ9.eyJuYmYiOjE2OTU3NDUYYDSsImV4cCI6MTY5NTc0NTc4NiwiaWF0IjoxNjk1NzQ1Nzg2LCJ1c2VybmFtZSI6IjIxMTExMjEyMjAifQ.lJI_CabcpDDvr8b_LbitKUkF7b8UHGknTvifx1RcDUC'
+            'eyJ0eXAiOiJKV1QiLCJhAbDciJIUzI1NiJ9.eyJuYmYiOjE2OTU3NDUYYDSsImV4cCI6MTY5NTc0NTc4NiwiaWF0IjoxNjk1NzQ1Nzg2LCJ1c2VybmFtZSI6IjIxMTExMjEyMjAifQ.lJI_CabcpDDvr8b_LbitKUkF7b8UHGknTvifx1RcDUC',
+          userInfo: {
+            username: '荏苒',
+            avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif?imageView2/1/w/80/h/80',
+            role: 'admin'
+          }
         },
         message: '登陆成功'
       }
