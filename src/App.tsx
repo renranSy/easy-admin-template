5d84c17b5f66bef84029fb9e88f0e68d0ed849f6
sunyang <2480901422@qq.com>
2023-10-21 14:18:23 +0800

    perf🚀: lazy动态引入组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
-import React from 'react'
+import React, { JSX, lazy, Suspense } from 'react'
 import './index.less'
 import { HashRouter, Route, Routes } from 'react-router-dom'
 import { App as AntdApp, ConfigProvider } from 'antd'
 import zhCN from 'antd/lib/locale/zh_CN'
-import Home from './views/home'
-import Test from './views/test'
-import Layouts from './layouts'
-import User from './views/user'
-import SystemInfo from './views/systemInfo'
-import One from '@/views/multiMenu/one'
-import PageOne from '@/views/multiMenu/two/pageOne'
-import PageTwo from '@/views/multiMenu/two/pageTwo'
-import Resource from '@/views/resource/resource'
-import Login from '@/views/login'
-import Error404 from '@/views/error/404'
-import Error500 from '@/views/error/403';
+
+const NLoading = lazy(() => import('@/components/NLoading'))
+const Layouts = lazy(() => import('@/layouts'))
+const User = lazy(() => import('@/views/user'))
+const Home = lazy(() => import('@/views/home'))
+const Test = lazy(() => import('@/views/test'))
+const SystemInfo = lazy(() => import('@/views/systemInfo'))
+const One = lazy(() => import('@/views/multiMenu/one'))
+const PageOne = lazy(() => import('@/views/multiMenu/two/pageOne'))
+const PageTwo = lazy(() => import('@/views/multiMenu/two/pageTwo'))
+const Resource = lazy(() => import('@/views/resource'))
+const Login = lazy(() => import('@/views/login'))
+const Error404 = lazy(() => import('@/views/error/404'))
+const Error403 = lazy(() => import('@/views/error/403'))
+
+const load = (children: JSX.Element) => <Suspense fallback={<NLoading />}>{children}</Suspense>
 
 function App() {
   return (
@@ -22,20 +26,20 @@ function App() {
       <ConfigProvider locale={zhCN}>
         <AntdApp>
           <Routes>
-            <Route path="/login" element={<Login />} />
+            <Route path="/login" element={load(<Login />)} />
             <Route path="/" element={<Layouts />}>
-              <Route path="/" element={<Home />} />
-              <Route path="/user" element={<User />} />
-              <Route path="/info" element={<SystemInfo />} />
-              <Route path="/resource" element={<Resource />} />
-              <Route path="/one" element={<One />} />
-              <Route path="/two/one" element={<PageOne />} />
-              <Route path="/two/two" element={<PageTwo />} />
-              <Route path="/404" element={<Error404 />} />
-              <Route path="/403" element={<Error500 />} />
+              <Route path="/" element={load(<Home />)} />
+              <Route path="/user" element={load(<User />)} />
+              <Route path="/info" element={load(<SystemInfo />)} />
+              <Route path="/resource" element={load(<Resource />)} />
+              <Route path="/one" element={load(<One />)} />
+              <Route path="/two/one" element={load(<PageOne />)} />
+              <Route path="/two/two" element={load(<PageTwo />)} />
+              <Route path="/404" element={load(<Error404 />)} />
+              <Route path="/403" element={load(<Error403 />)} />
             </Route>
-            <Route path="/test" element={<Test />} />
-            <Route path="*" element={<Error404 />} />
+            <Route path="/test" element={load(<Test />)} />
+            <Route path="*" element={load(<Error404 />)} />
           </Routes>
         </AntdApp>
       </ConfigProvider>
