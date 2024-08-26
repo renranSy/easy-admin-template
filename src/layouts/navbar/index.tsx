97ad89faa3554ede06656708302047a3d3371a14
sunyang <2480901422@qq.com>
2024-08-26 23:29:54 +0800

    feat: tab context menu

--- src/layouts/navbar/index.tsx
+++ src/layouts/navbar/index.tsx
@@ -1,13 +1,45 @@
 import React from 'react'
 import './index.less'
-import { Tab, useTab } from '@/hooks/useTab'
+import { DefaultTab, Tab, useTab } from '@/hooks/useTab'
 import { useAutoAnimate } from '@formkit/auto-animate/react'
 import { Space } from 'antd'
 import { IconX } from '@tabler/icons-react'
+import IContextMenuWrapper from '../../components/IContextMenuWrapper'
+import IContextMenuItem, { IContextMenuOption } from '@/components/IContextMenuWrapper/IContextMenuItem'
 
 export const Navbar = () => {
-  const { activeTab, tabList, clickTab, closeTab } = useTab()
+  const { activeTab, tabList, clickTab, closeTab, closeTabList } = useTab()
   const [parent] = useAutoAnimate()
+
+  const options: IContextMenuOption[] = [
+    {
+      key: 'close',
+      name: '关闭',
+      icon: 'x',
+      divider: true
+    },
+    {
+      key: 'closeLeft',
+      name: '关闭左侧标签页',
+      icon: 'arrowBarToLeft'
+    },
+    {
+      key: 'closeRight',
+      name: '关闭右侧标签页',
+      icon: 'arrowBarToRight',
+      divider: true
+    },
+    {
+      key: 'closeOther',
+      name: '关闭其他标签页',
+      icon: 'arrowBarBoth'
+    },
+    {
+      key: 'closeAll',
+      name: '关闭所有标签页',
+      icon: 'arrowsRightLeft'
+    }
+  ]
 
   const Close = (props: { tab: Tab }) => {
     if (props.tab.pathname === '/' && tabList.length === 1) {
@@ -22,20 +54,89 @@ export const Navbar = () => {
     )
   }
 
+  const onSetOptions = (activeKey: string) => {
+    return options.map((item) => {
+      let disabled = false
+      const index = tabList.findIndex((item) => {
+        return item.pathname === activeKey
+      })
+      switch (item.key) {
+        case 'closeLeft':
+          if (index === 0) {
+            disabled = true
+          }
+          break
+        case 'closeRight':
+          if (index === tabList.length - 1) {
+            disabled = true
+          }
+          break
+        case 'closeOther':
+          if (tabList.length === 1) {
+            disabled = true
+          }
+          break
+        case 'closeAll':
+          if (tabList.length === 1 && activeTab.pathname === DefaultTab.pathname) {
+            disabled = true
+          }
+      }
+      return {
+        ...item,
+        disabled
+      }
+    })
+  }
+
+  const onClickMenu = (eventName: string, activeKey: string) => {
+    const tabIndex = tabList.findIndex((item) => {
+      return item.pathname === activeKey
+    })
+
+    switch (eventName) {
+      case 'closeLeft':
+        closeTabList(
+          tabList.filter((_, index) => index < tabIndex),
+          activeKey
+        )
+        break
+      case 'closeRight':
+        closeTabList(
+          tabList.filter((_, index) => index > tabIndex),
+          activeKey
+        )
+        break
+      case 'closeOther':
+        closeTabList(
+          tabList.filter((_, index) => index !== tabIndex),
+          activeKey
+        )
+        break
+      case 'closeAll':
+        closeTabList(tabList, activeKey)
+        break
+    }
+  }
+
   return (
     <div className="nav-container px-3 box-border shadow-md bg-white flex items-center text-nowrap">
-      <Space ref={parent}>
-        {tabList.map((t) => (
-          <div
-            key={t.pathname}
-            className={
-              'tab cursor-pointer flex items-center ' + (t.pathname === activeTab.pathname ? 'active-tab' : '')
-            }>
-            <span onClick={() => clickTab(t)}>{t.label}</span>
-            <Close tab={t} />
-          </div>
-        ))}
-      </Space>
+      <IContextMenuWrapper onClick={onClickMenu}>
+        <Space ref={parent}>
+          {tabList.map((t) => (
+            <IContextMenuItem
+              options={options}
+              activeKey={t.pathname}
+              key={t.pathname}
+              onSetOptions={onSetOptions}
+              className={
+                'tab cursor-pointer flex items-center ' + (t.pathname === activeTab.pathname ? 'active-tab' : '')
+              }>
+              <span onClick={() => clickTab(t)}>{t.label}</span>
+              <Close tab={t} />
+            </IContextMenuItem>
+          ))}
+        </Space>
+      </IContextMenuWrapper>
     </div>
   )
 }
