d1c2e54105f05f53978c7b4ba70d2bda401dfcf6
Yang Sun <2480901422@qq.com>
2024-10-06 00:23:28 +0800

    save

--- src/views/feature/richEditor/index.tsx
+++ src/views/feature/richEditor/index.tsx
@@ -1,42 +1,14 @@
-import React, { useEffect, useState } from 'react'
-import { IDomEditor, IEditorConfig, IToolbarConfig } from '@wangeditor/editor'
-import { Editor, Toolbar } from '@wangeditor/editor-for-react'
-import '@wangeditor/editor/dist/css/style.css'
+import React, { useState } from 'react'
+import '@renransy/rq-editor/lib/index.css'
+import RQEditor from '@renransy/rq-editor'
 
 const RichEditor = () => {
-  const [editor, setEditor] = useState<IDomEditor | null>(null)
-
-  const [html, setHtml] = useState('<p>Hello, world!</p>')
-
-  const toolbarConfig: Partial<IToolbarConfig> = {}
-  const editorConfig: Partial<IEditorConfig> = {
-    placeholder: '请输入内容'
-  }
-
-  useEffect(() => {
-    return () => {
-      if (editor !== null) {
-        editor.destroy()
-        setEditor(null)
-      }
-    }
-  }, [])
+  const [value, setValue] = useState('')
   return (
     <>
       <div>
-        <Toolbar editor={editor} defaultConfig={toolbarConfig} mode="default" />
-        <Editor
-          defaultConfig={editorConfig}
-          value={html}
-          onCreated={setEditor}
-          onChange={(editor) => {
-            setHtml(editor.getHtml())
-          }}
-          mode="default"
-          style={{ height: '500px', overflowY: 'hidden' }}
-        />
+        {/*<RQEditor value={value} />*/}
       </div>
-      <div style={{ marginTop: '15px' }}>{html}</div>
     </>
   )
 }
